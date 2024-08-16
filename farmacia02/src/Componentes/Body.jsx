e3d7c365f9415199adf03650aa88c5478688775a
Zuma Elma Pereira <zumapereira@hotmail.com>
2024-08-16 20:28:06 -0300

    ok

--- farmacia02/src/Componentes/Body.jsx
+++ farmacia02/src/Componentes/Body.jsx
@@ -2,10 +2,13 @@ import'./Body.css'
 import React from 'react'
 import Galeria from './Galeria'
 
+
 export default function Body() {
   return (
     <div className='body'>
       <Galeria/>
+     
+      
     </div>
   )
 }
--- farmacia02/src/Componentes/Galeria.jsx
+++ farmacia02/src/Componentes/Galeria.jsx
@@ -3,14 +3,14 @@ import './Galeria.css'
 
 export default function Galeria() {
   return (
-    <div >
+    <div className="container-galeria">
+    <img 
+      className="imagem-placa" 
+      src="/image/placa.png" 
+      alt="Placa da Farmácia" 
+    />
+  </div>
 
-                <img 
-                    className='imagem-placa' 
-                    src="./public/image/placa.png"  
-                    alt="Placa da Farmácia" 
-                />
-
-    </div>
+    
   )
 }
