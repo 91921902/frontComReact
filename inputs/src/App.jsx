f671e5490670814445c758d4d37c622b1d4251f3
Zuma Elma Pereira <zumapereira@hotmail.com>
2024-08-27 16:50:43 -0300

    pronto

---
+++ inputs/src/App.jsx
@@ -0,0 +1,78 @@
+
+import './App.css'
+import { useState } from 'react';
+
+function App() {
+  const[inputNome,  setInputNome] = useState('');
+  const [inputIdade, setInputIdade] = useState(0);
+  const[usuarios, setUsuarios] = useState([]);
+
+  function mudouNome(event) {
+    setInputNome(event.target.value)
+  }
+  function mudouIdade(event) {
+    setInputIdade(event.target.value)
+  }
+  function cadastrarUsuario() {
+        
+        let usuario ={
+          id: Date.now(),
+          nome:inputNome,
+          idade:inputIdade
+        }
+        console.log(usuario);
+        setUsuarios([...usuarios,usuario])
+
+        setInputNome('')
+        setInputIdade('')
+  }
+  
+  return (
+    <>
+
+      <h1>Inputs Controlados</h1>
+
+
+      <div className="inputContainer">
+        <label htmlFor="inpNome">Nome:</label>
+        <input
+          value={inputNome}
+          onChange={mudouNome}
+         id='inpNome' type="text"/>
+      </div>
+
+       <div className="inputContainer">
+         <label htmlFor="inpIdade">Idade:</label>
+         <input
+         value={inputIdade}
+         onChange={mudouIdade}
+         type='text'id='inpIdade'/>
+       </div>
+
+       <button onClick={cadastrarUsuario}>Cadastrar</button>
+
+      <div>
+        <p>
+          Nome Digitado: {inputNome}
+        </p>
+        <p>
+          Idade: {inputIdade}
+        </p>
+        <div>
+          Usuarios:{
+          usuarios.map((usuario,index)=>(
+            <div key={usuario.id}>
+            <p>{usuario.nome}</p>
+            <p>{usuario.idade}</p>
+            </div>
+          ))}
+          
+        </div>
+        
+      </div>
+
+    </>
+  )
+}
+
+export default App
